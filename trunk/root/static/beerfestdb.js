f43de2dca57ceabef31a339a9cc5d9426ce76a19
tfrayner <tfrayner@dd1a25d9-2848-0410-9cba-b326786b0dac>
2009-10-23 23:37:49 +0000

    Early experiments in using ExtJS to provide a grid-based record editing system.

---
+++ trunk/root/static/beerfestdb.js
@@ -0,0 +1,114 @@
+/*
+ * $Id$
+ */
+
+Ext.onReady(function(){
+
+        function submitChanges( data ) {
+            Ext.Ajax.request({
+                    url:        posturl,
+                    success:    function() { store.reload() },
+                    params:     { changes: Ext.util.JSON.encode( data ) }
+                });
+        }
+
+        var col_model = new Ext.grid.ColumnModel([
+        {
+            id:         'product_id',
+            header:     'ID',
+            dataIndex:  'product_id',
+            width:      25
+        },
+        {
+            id:         'name',
+            header:     'Name',
+            dataIndex:  'name',
+            width:      25,
+            editor:     new Ext.form.TextField({
+                    allowBlank:     false,
+                })
+        },
+        {
+            id:         'description',
+            header:     'Description',
+            dataIndex:  'description',
+            width:      100
+        },
+        {
+            id:         'comment',
+            header:     'Comment',
+            dataIndex:  'comment',
+            width:      100
+        }
+                                                  ]); 
+        
+        col_model.defaultSortable = true;
+
+        var Product = Ext.data.Record.create([
+        { name: 'product_id',  type: 'int' },
+        { name: 'name',        type: 'string' },
+        { name: 'description', type: 'string' },
+        { name: 'comment',     type: 'string' }
+                                             ]);
+
+        var store = new Ext.data.JsonStore({
+                url:        gridurl,
+                root:       'products',
+                fields:     Product
+            });
+
+        var grid = new Ext.grid.EditorGridPanel({
+                store:              store,
+                cm:                 col_model,
+                title:              'Edit Products',
+                width:              800,
+                height:             400,
+                frame:              true,
+                autoExpandColumn:   'name',
+                renderTo:           'datagrid',
+                tbar:
+                [
+        {               
+            text:           'New Product',
+            handler:        function() {
+                var p = new Product({
+                        name:           'Unnamed New Product',
+                        description:    'Unknown',
+                    }); 
+                grid.stopEditing();
+                store.insert( 0, p );
+                grid.startEditing( 0, 1 );
+            },
+        },
+        {
+            text:           'Save Changes',
+            handler:        function() {
+                grid.stopEditing();
+                var changes = new Array();
+                var dirty = store.getModifiedRecords();
+                for ( var i = 0 ; i < dirty.length ; i++ ) {
+                    var id = dirty[i].get( 'product_id' );
+                    var fields = dirty[i].getChanges();
+                    fields.product_id = dirty[i].get( 'product_id' );
+                    changes.push( fields );
+                }
+                //                console.log( changes );
+                submitChanges( changes );
+                store.commitChanges();
+            },
+        },
+        {
+            text:           'Discard Changes',
+            handler:        function() {
+                grid.stopEditing();
+                store.rejectChanges();
+            },
+        }
+                 ]
+                
+            });
+
+        store.load();
+
+    });
+
