3fce092263e0781f0d37edc98b5bbc74453dce8c
tfrayner <tfrayner@dd1a25d9-2848-0410-9cba-b326786b0dac>
2009-11-01 11:42:52 +0000

    Further refactoring, separating out the toolbar button objects.

--- trunk/root/static/product.js
+++ trunk/root/static/product.js
@@ -28,23 +28,23 @@ function deleteProducts( data, url, store ) {
 
 NewButton = Ext.extend(Ext.Button, {
 
-    text: 'New Row',
+    text:    'New Row',
+    tooltip: 'Add a new row to the table',
+    iconCls: 'icon-plus',
 
     initComponent: function() {
         Ext.apply(this,
-            {               
-                tooltip: 'Add a new row to the table',
-                handler: function() {
-                    var p = new this.store.recordType();
-                    this.grid.stopEditing();
-                    this.store.insert( 0, p );
-                    this.grid.startEditing( 0, 1 );
-                },
-                iconCls: 'icon-plus',
-                scope: this,
-            }
-        );
-
+                  {               
+                      handler: function() {
+                          var p = new this.grid.store.recordType();
+                          this.grid.stopEditing();
+                          this.grid.store.insert( 0, p );
+                          this.grid.startEditing( 0, 1 );
+                      },
+                      scope: this,
+                  }
+                 );
+        
         NewButton.superclass.initComponent.apply(this, arguments);
     },
     
@@ -53,13 +53,120 @@ NewButton = Ext.extend(Ext.Button, {
     },
 });
 
-ProductGrid = Ext.extend(Ext.grid.EditorGridPanel, {
+SaveButton = Ext.extend(Ext.Button, {
+    
+    text:     'Save Changes',
+    tooltip:  'Write changes to the database',
+    iconCls:  'icon-save-table',
+
+    initComponent: function() {
+        Ext.apply(this,
+                  {
+                      handler:  function() {
+                          this.grid.stopEditing();
+                          var changes = new Array();
+                          var dirty = this.grid.store.getModifiedRecords();
+                          for ( var i = 0 ; i < dirty.length ; i++ ) {
+                              changes.push( this.getFields(dirty[i]) );
+                          }
+                          submitChanges( changes, posturl, this.grid.store );
+                          this.grid.store.commitChanges();
+                      },
+                      scope: this,
+                  }
+                 );
+
+        SaveButton.superclass.initComponent.apply(this, arguments);
+    },
+
+    onRender: function() {
+        SaveButton.superclass.onRender.apply(this, arguments);
+    },
+});
+
+DiscardButton = Ext.extend(Ext.Button, {
+
+    text:           'Discard Changes',
+    tooltip:        'Restore the previously saved version',
+    iconCls: 'icon-cancel',
+
+    initComponent: function() {
+        
+        Ext.apply(this,
+                  {
+                      handler:        function() {
+                          this.grid.stopEditing();
+                          this.grid.store.rejectChanges();
+                      },
+                      scope: this,
+                  }
+                 );
+
+        DiscardButton.superclass.initComponent.apply(this, arguments);
+    },
+
+    onRender: function() {
+        DiscardButton.superclass.onRender.apply(this, arguments);
+    },
+});
+
+RemoveButton = Ext.extend(Ext.Button, {
+
+    text:           'Remove Rows',
+    tooltip:        'Remove the selected item(s)',
+    disabled:       true,
+    iconCls:        'icon-minus',
+
+    initComponent: function() {
+
+        Ext.apply(
+            this,
+            {
+                handler:        function() {
+                    this.grid.stopEditing();
+                    Ext.Msg.show({
+                        title:    'Delete',
+                        msg:      'Really delete the selected rows?',
+                        buttons:  Ext.Msg.YESNO,
+                        icon:     Ext.MessageBox.QUESTION,
+                        fn:       function(btn, text){
+                            if (btn == 'yes'){
+                                var changes = new Array();
+                                var dirty   = this.sm.getSelections();
+                                for ( var i = 0 ; i < dirty.length ; i++ ) {
+                                    var id = dirty[i].get( this.idfield );
+                                    changes.push( id );
+                                }
+                                deleteProducts( changes,
+                                                this.deleteurl,
+                                                this.grid.store );
+                                this.grid.store.reload();
+                            }
+                        },
+                        scope: this,
+                    });
+                },
+                scope: this,
+            }
+        );
+        
+        RemoveButton.superclass.initComponent.apply(this, arguments);
+    },
+    
+    onRender: function() {
+        RemoveButton.superclass.onRender.apply(this, arguments);
+    },
+});
+
+
+MyEditorGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 
     autoExpandColumn:   'name',
     renderTo:           'datagrid',
     columnLines:        true,
 
     initComponent: function() {
+
         var sm = new Ext.grid.CheckboxSelectionModel({
             listeners: {
                 // On selection change, set enabled state of the removeButton
@@ -80,153 +187,54 @@ ProductGrid = Ext.extend(Ext.grid.EditorGridPanel, {
             keepSelection:true,
             actions:[{
                 iconCls:'icon-open',
-                tooltip:'View product details',
+                tooltip:'View details',
             }],
         });
 
         action.on({
-            action:function(grid, record, action, row, col) {
-                var t = new Ext.XTemplate('/product/view/{product_id}');
-                t.compile();
-                window.location=t.apply({product_id: record.get('product_id')});
-            },
+            action: this.viewLink,
         });
 
-        var style_combo = new Ext.form.ComboBox({
-            typeAhead:      true,
-            triggerAction:  'all',
-            transform:      'stylepopup',
-            lazyRender:     true,
-            listClass:      'x-combo-list-small'
-        });
-        
         var col_model = new Ext.grid.ColumnModel({
             defaults: {
                 sortable: true
             },
-            columns: [
-                sm,
-                { id:         'name',
-                  header:     'Name',
-                  dataIndex:  'name',
-                  width:      25,
-                  editor:     new Ext.form.TextField({
-                      allowBlank:     false,
-                  })},
-                { id:         'description',
-                  header:     'Description',
-                  dataIndex:  'description',
-                  width:      150,
-                  editor:     new Ext.form.TextField({
-                      allowBlank:     true,
-                  })},
-                { id:         'comment',
-                  header:     'Comment',
-                  dataIndex:  'comment',
-                  width:      150,
-                  editor:     new Ext.form.TextField({
-                      allowBlank:     true,
-                  })},
-                { id:         'product_style_id',
-                  header:     'Style',
-                  dataIndex:  'product_style_id',
-                  width:      100,
-                  renderer:   function(value) {  // render the option text, not the value.
-                      var r = style_combo.store.getById(value); 
-                      return r ? r.get('text') : '<unknown>';
-                  },
-                  editor:     style_combo },
-                action,
-            ],
+            columns: [].concat(sm, this.content_cols, action),
         }); 
         
-        var Product = Ext.data.Record.create([
-            { name: 'product_id',       type: 'int' },
-            { name: 'name',             type: 'string',   allowBlank: false },
-            { name: 'description',      type: 'string' },
-            { name: 'comment',          type: 'string' },
-            { name: 'product_style_id', type: 'int' }
-        ]);
-
-        var store = new Ext.data.JsonStore({
-            url:        gridurl,
-            root:       'objects',
-            fields:     Product
-        });
-
         Ext.apply(this, {
-            store:              store,
             cm:                 col_model,
             sm:                 sm,
             plugins:            action,
             tbar:
             [
                 new NewButton({text:   'New Product',
-                               grid:   this,
-                               store:  store}),
-                {
-                    text:           'Save Changes',
-                    tooltip:        'Write changes to the database',
-                    handler:        function() {
-                        this.stopEditing();
-                        var changes = new Array();
-                        var dirty = store.getModifiedRecords();
-                        for ( var i = 0 ; i < dirty.length ; i++ ) {
-                            var id = dirty[i].get( 'product_id' );
-                            var fields = dirty[i].getChanges();
-                            fields.product_id = dirty[i].get( 'product_id' );
-                            fields.product_category_id = category_id;
-                            changes.push( fields );
-                        }
-                        submitChanges( changes, posturl, store );
-                        store.commitChanges();
+                               grid:   this}),
+                new SaveButton({
+                    grid:      this,
+                    getFields: function(record) {
+                        var fields = record.getChanges();
+                        fields.product_id = record.get( 'product_id' );
+                        fields.product_category_id = category_id;
+                        return(fields);
                     },
-                    iconCls: 'icon-save-table',
-                    scope: this,
-                },
-                {
-                    text:           'Discard Changes',
-                    tooltip:        'Restore the previously saved version',
-                    handler:        function() {
-                        this.stopEditing();
-                        store.rejectChanges();
-                    },
-                    iconCls: 'icon-cancel',
-                    scope: this,
-                },
-                {
-                    text:           'Remove Products',
-                    tooltip:        'Remove the selected item(s)',
-                    ref:            '../removeButton',
-                    disabled:       true,
-                    handler:        function() {
-                        this.stopEditing();
-                        Ext.Msg.confirm('Name', 'Really delete the selected rows?',
-                                        function(btn, text){
-                                            if (btn == 'yes'){
-                                                var changes = new Array();
-                                                var dirty = sm.getSelections();
-                                                for ( var i = 0 ; i < dirty.length ; i++ ) {
-                                                    var id = dirty[i].get( 'product_id' );
-                                                    changes.push( id );
-                                                }
-                                                deleteProducts( changes, deleteurl, store );
-                                                store.reload();
-                                            }
-                                        });
-                    },
-                    iconCls: 'icon-minus',
-                    scope: this,
-                }
+                }),
+                new DiscardButton({grid:this}),
+                new RemoveButton({text:         'Remove Products',
+                                  grid:         this,
+                                  sm:           sm,
+                                  ref:          '../removeButton',
+                                  idfield:      'product_id',
+                                  deleteurl:    deleteurl}),
             ]
             
         });
-        ProductGrid.superclass.initComponent.apply(this, arguments);
+        MyEditorGrid.superclass.initComponent.apply(this, arguments);
     },
     
     onRender: function() {
         this.store.load();
-        ProductGrid.superclass.onRender.apply(this, arguments);
+        MyEditorGrid.superclass.onRender.apply(this, arguments);
     }
 });
 
@@ -235,10 +243,72 @@ Ext.onReady(function(){
     // Enable tooltips
     Ext.QuickTips.init();
     
+    var Product = Ext.data.Record.create([
+        { name: 'product_id',       type: 'int' },
+        { name: 'name',             type: 'string',   allowBlank: false },
+        { name: 'description',      type: 'string' },
+        { name: 'comment',          type: 'string' },
+        { name: 'product_style_id', type: 'int' }
+    ]);
+
+    var store = new Ext.data.JsonStore({
+        url:        gridurl,
+        root:       'objects',
+        fields:     Product
+    });
+
+    var style_combo = new Ext.form.ComboBox({
+        typeAhead:      true,
+        triggerAction:  'all',
+        transform:      'stylepopup',
+        lazyRender:     true,
+        listClass:      'x-combo-list-small'
+    });
+    
+    var content_cols = [
+        { id:         'name',
+          header:     'Name',
+          dataIndex:  'name',
+          width:      25,
+          editor:     new Ext.form.TextField({
+              allowBlank:     false,
+          })},
+        { id:         'description',
+          header:     'Description',
+          dataIndex:  'description',
+          width:      150,
+          editor:     new Ext.form.TextField({
+              allowBlank:     true,
+          })},
+        { id:         'comment',
+          header:     'Comment',
+          dataIndex:  'comment',
+          width:      150,
+          editor:     new Ext.form.TextField({
+              allowBlank:     true,
+          })},
+        { id:         'product_style_id',
+          header:     'Style',
+          dataIndex:  'product_style_id',
+          width:      100,
+          renderer:   function(value) {  // render the option text, not the value.
+              var r = style_combo.store.getById(value); 
+              return r ? r.get('text') : '<unknown>';
+          },
+          editor:     style_combo },
+    ];
+
+    function viewLink (grid, record, action, row, col) {
+        var t = new Ext.XTemplate('/product/view/{product_id}');
+        window.location=t.apply({product_id: record.get('product_id')});
+    };
+
     var panel = new Ext.Panel({
         title: festivalname + ' product listing: ' + categoryname,
         layout: 'fit',
-        items: new ProductGrid(),
+        items: new MyEditorGrid({store:        store,
+                                 content_cols: content_cols,
+                                 viewLink:     viewLink,}),
         tbar:
         [
             { text: 'Home', handler: function() { window.location = '/'; } },
