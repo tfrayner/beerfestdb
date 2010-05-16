cc032ca29c96a1c5acd15bc2d6a5925f70f3cedb
tfrayner <tfrayner@dd1a25d9-2848-0410-9cba-b326786b0dac>
2010-05-16 20:05:36 +0000

    Adding proper primary key columns to company_product and festival_product, since it turns out that these linking tables are likely to develop further.

--- trunk/lib/BeerFestDB/ORM/CompanyProduct.pm
+++ trunk/lib/BeerFestDB/ORM/CompanyProduct.pm
@@ -8,12 +8,14 @@ use base 'DBIx::Class';
 __PACKAGE__->load_components("Core");
 __PACKAGE__->table("company_product");
 __PACKAGE__->add_columns(
+  "company_product_id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 3 },
   "company_id",
   { data_type => "INT", default_value => undef, is_nullable => 0, size => 3 },
   "product_id",
   { data_type => "INT", default_value => undef, is_nullable => 0, size => 3 },
 );
-__PACKAGE__->set_primary_key("company_id", "product_id");
+__PACKAGE__->set_primary_key("company_product_id");
 __PACKAGE__->belongs_to(
   "company_id",
   "BeerFestDB::ORM::Company",
@@ -26,8 +28,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-05-02 20:33:33
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4A2f9Lgweo09ldCMZ+k2yw
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-05-16 21:03:27
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:mPcw5rCYOtAHYleWX+lqkA
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
