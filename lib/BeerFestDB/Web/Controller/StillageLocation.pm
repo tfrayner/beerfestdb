206ed8616f90ee73dd227fdc9705be794e4a10e9
Tim Rayner <tfrayner@users.noreply.github.com>
2010-05-16 16:52:14 +0000

    Ongoing work on adding stillage and bar locations to the web site.

---
+++ lib/BeerFestDB/Web/Controller/StillageLocation.pm
@@ -0,0 +1,110 @@
+package BeerFestDB::Web::Controller::StillageLocation;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+BeerFestDB::Web::Controller::StillageLocation - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched BeerFestDB::Web::Controller::StillageLocation in StillageLocation.');
+}
+
+# FIXME we need a means to edit and view casks on the stillage.
+
+sub add_to_festival : Local {
+
+    my ( $self, $c, $festival_id ) = @_;
+
+    $c->stash()->{festival_id} = $festival_id;
+}
+
+sub submit : Local {
+
+    my ( $self, $c ) = @_;
+
+    eval {
+        $c->model('DB::StillageLocation')->update_or_create(
+            $c->request->params,
+        );
+    };
+    if ($@) {
+        $c->response->status('403');  # Forbidden
+
+        # N.B. flash_to_stash doesn't seem to work for JSON views.
+        $c->stash->{error} = 'Unable to save one or more objects to database';
+    }
+
+    return;
+}
+
+sub grid : Local {
+
+    my ( $self, $c, $stillage_id ) = @_;
+
+    if ( defined $stillage_id ) {
+        my $stillage = $c->model('DB::StillageLocation')->find($stillage_id);
+        unless ( $stillage ) {
+            $c->flash->{error} = "Error: Stillage not found.";
+            $c->res->redirect( $c->uri_for('/default') );
+            $c->detach();        
+        }
+        $c->stash->{stillage} = $stillage;
+    }
+
+    return;
+}
+
+sub list_casks : Local {
+
+    my ( $self, $c, $id ) = @_;
+
+    my $rs = $c->model( 'DB::Cask' )->search({ stillage_location_id => $id });
+    my @casks;
+    while ( my $cask = $rs->next ) {
+        push( @casks, {
+            cask_id     => $cask->cask_id,
+            product     => $cask->gyle_id->product_id->name,
+        } );
+    }
+
+    $c->stash->{ 'objects' } = \@casks;
+    $c->detach( $c->view( 'JSON' ) );
+
+    return;
+
+
+}
+
+=head1 AUTHOR
+
+Tim Rayner
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# Uncomment when ready FIXME; currently blocks server restart.
+#__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ root/static/js/forms/add_stillage_to_festival.js
@@ -0,0 +1,66 @@
+/* 
+ * $Id$
+ */
+
+// This function needs serious work FIXME.
+
+Ext.onReady(function(){
+
+    Ext.QuickTips.init();
+
+    // turn on validation errors beside the field globally
+    Ext.form.Field.prototype.msgTarget = 'side';
+
+    var bd = Ext.getBody();
+
+    /*
+     * ================  Simple form  =======================
+     */
+    bd.createChild({tag: 'h2', html: 'Add a new stillage location'});
+
+
+    var formObject;
+
+    var saveHandler = function(button, event) {
+        // FIXME handle failure as well.
+        formObject.form.submit({ waitMsg: 'Saving stillage details...',
+                                 success: doneFunction } );
+    };
+
+    var doneFunction = function(form, action) {
+        alert('success')
+//        window.location = '/festival/grid'
+    };
+
+    formObject = new Ext.FormPanel({
+        labelWidth: 100, // label settings here cascade unless overridden
+        url:       posturl,
+        frame:     true,
+        title:     'Stillage details',
+        bodyStyle: 'padding:5px 5px 0',
+        width:     350,
+        defaults:  {width: 200},
+        defaultType: 'textfield',
+
+        items: [
+            {
+                fieldLabel: 'Stillage Name',
+                name: 'description',
+                allowBlank:false
+            },{
+                name:   'festival_id',
+                value:  festival,
+                hidden: true
+            }
+        ],
+
+        buttons: [{
+            text: 'Save',
+            handler: saveHandler
+        },{
+            text: 'Cancel',
+        }]
+    });
+
+    formObject.render(document.body);
+});
