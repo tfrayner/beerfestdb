2705abc5aa83baedc94c5405690324a16ffd6715
tfrayner <tfrayner@dd1a25d9-2848-0410-9cba-b326786b0dac>
2010-12-26 22:07:11 +0000

    Updating the a grid view for casks on a given stillage to include actual useful information.

--- trunk/lib/BeerFestDB/Web/Controller/Cask.pm
+++ trunk/lib/BeerFestDB/Web/Controller/Cask.pm
@@ -16,6 +16,40 @@ Catalyst Controller.
 
 =cut
 
+sub BUILD {
+
+    my ( $self, $params ) = @_;
+
+    $self->model_view_map({
+        cask_id           => 'cask_id',
+        festival_id       => 'festival_id',
+        distributor_id    => 'distributor_company_id',
+        container_size_id => 'container_size_id',
+        bar_id            => 'bar_id',
+        stillage_id       => 'stillage_location_id',
+        currency_id       => 'currency_id',
+        price             => 'price',
+        gyle_id           => 'gyle_id',
+        product_id        => {
+            gyle_id  => 'product_id',
+        },
+        company_id        => {
+            gyle_id  => {
+                product_id => 'company_id',
+            },
+        },
+        stillage_bay      => 'stillage_bay',
+        stillage_x        => 'stillage_x_location',
+        stillage_y        => 'stillage_y_location',
+        stillage_z        => 'stillage_z_location',
+        comment           => 'comment',
+        ext_reference     => 'external_reference',
+        int_reference     => 'internal_reference',
+        is_vented         => 'is_vented',
+        is_tapped         => 'is_tapped',
+        is_ready          => 'is_ready',
+    });
+}
 
 =head2 index
 
@@ -27,10 +61,48 @@ sub index :Path :Args(0) {
     $c->response->body('Matched BeerFestDB::Web::Controller::Cask in Cask.');
 }
 
+sub list_by_stillage : Local {
+
+    my ( $self, $c, $id ) = @_;
+
+    my $rs = $c->model( 'DB::Cask' )->search({ stillage_location_id => $id });
+
+    $self->generate_json_and_detach( $c, $rs );
+}
+
+=head2 submit
+
+=cut
+
+sub submit : Local {
+
+    my ( $self, $c ) = @_;
+
+    my $rs = $c->model( 'DB::Cask' );
+
+    $self->write_to_resultset( $c, $rs );
+}
+
+=head2 delete
+
+=cut
+
+sub delete : Local {
+
+    my ( $self, $c ) = @_;
+
+    my $rs = $c->model( 'DB::Cask' );
+
+    ## FIXME perhaps a custom delete_from_stillage method?
+    die("Actually we probably just want to break the cask/stillage link here");
+    
+    $self->delete_from_resultset( $c, $rs );
+}
+
 
 =head1 AUTHOR
 
-Tim Rayner,,,,
+Tim Rayner
 
 =head1 LICENSE
 
--- trunk/root/static/js/grids/casks.js
+++ trunk/root/static/js/grids/casks.js
@@ -8,8 +8,18 @@ Ext.onReady(function(){
     Ext.QuickTips.init();
     
     var Cask = Ext.data.Record.create([
-        { name: 'cask_id',       type: 'int' },
-        { name: 'product',       type: 'string' },
+        { name: 'cask_id',           type: 'int' },
+        { name: 'festival_id',       type: 'int' },
+        { name: 'company_id',        type: 'int' },
+        { name: 'product_id',        type: 'int' },
+        { name: 'container_size_id', type: 'int' },
+        { name: 'stillage_id',       type: 'int' },
+        { name: 'gyle_id',           type: 'int' },
+        { name: 'int_reference',     type: 'string' },
+        { name: 'comment',           type: 'string' },
+        { name: 'is_vented',         type: 'int' },
+        { name: 'is_tapped',         type: 'int' },
+        { name: 'is_ready',          type: 'int' },
     ]);
 
     var store = new Ext.data.JsonStore({
@@ -18,13 +28,81 @@ Ext.onReady(function(){
         fields:     Cask
     });
 
+    var brewer_store = new Ext.data.JsonStore({
+        url:        url_supplier_list,
+        root:       'objects',
+        fields:     [{ name: 'company_id', type: 'int'    },
+                     { name: 'name',       type: 'string' }],
+    });
+    var brewer_combo = new Ext.form.ComboBox({
+        store:          brewer_store,
+        valueField:     'company_id',
+        displayField:   'name',
+        lazyRender:     true,
+    });
+    brewer_store.load();
+
+    var product_store = new Ext.data.JsonStore({
+        url:        url_product_list,
+        root:       'objects',
+        fields:     [{ name: 'product_id', type: 'int'    },
+                     { name: 'name',       type: 'string' }],
+    });
+    var product_combo = new Ext.form.ComboBox({
+        store:          product_store,
+        valueField:     'product_id',
+        displayField:   'name',
+        lazyRender:     true,
+    });
+    product_store.load();
+
     var content_cols = [
-        { id:         'name',
-          header:     'Name',
-          dataIndex:  'name',
-          width:      300,
+        { id:         'company_id',
+          header:     'Brewer',
+          dataIndex:  'company_id',
+          width:      130,
+          renderer:   MyComboRenderer(brewer_combo),
+          editable:   false, },            
+        { id:         'product_id',
+          header:     'Product',
+          dataIndex:  'product_id',
+          width:      130,
+          renderer:   MyComboRenderer(product_combo),
+          editable:     false, },
+        { id:         'int_reference',
+          header:     'Cask No.',
+          dataIndex:  'int_reference',
+          width:      50,
           editor:     new Ext.form.TextField({
-              allowBlank:     false,
+              allowBlank:     true,
+          })},
+        { id:         'is_vented',
+          header:     'Vented',
+          dataIndex:  'is_vented',
+          width:      50,
+          renderer:   MyCheckboxRenderer(),
+          editor:     new Ext.form.Checkbox({
+          })},
+        { id:         'is_tapped',
+          header:     'Tapped',
+          dataIndex:  'is_tapped',
+          width:      50,
+          renderer:   MyCheckboxRenderer(),
+          editor:     new Ext.form.Checkbox({
+          })},
+        { id:         'is_ready',
+          header:     'Ready',
+          dataIndex:  'is_ready',
+          width:      50,
+          renderer:   MyCheckboxRenderer(),
+          editor:     new Ext.form.Checkbox({
+          })},
+        { id:         'comment',
+          header:     'Comment',
+          dataIndex:  'comment',
+          width:      150,
+          editor:     new Ext.form.TextField({
+              allowBlank:     true,
           })},
     ];
 
@@ -35,7 +113,7 @@ Ext.onReady(function(){
 
     function recordChanges (record) {
         var fields = record.getChanges();
-        fields.product_id = record.get( 'cask_id' );
+        fields.cask_id = record.get( 'cask_id' );
         return(fields);
     }
 
