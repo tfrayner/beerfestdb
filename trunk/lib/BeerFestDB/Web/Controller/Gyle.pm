75cb0be13c6b6edbc311d8e8343f2dd1caf45bff
tfrayner <tfrayner@dd1a25d9-2848-0410-9cba-b326786b0dac>
2011-01-07 23:58:23 +0000

    Added a cask tab to the festival product view. Also added a Gyle controller.

---
+++ trunk/lib/BeerFestDB/Web/Controller/Gyle.pm
@@ -0,0 +1,122 @@
+#
+# This file is part of BeerFestDB, a beer festival product management
+# system.
+# 
+# Copyright (C) 2010 Tim F. Rayner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+
+package BeerFestDB::Web::Controller::Gyle;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'BeerFestDB::Web::Controller'; }
+
+=head1 NAME
+
+BeerFestDB::Web::Controller::Gyle - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+sub BUILD {
+
+    my ( $self, $params ) = @_;
+
+    $self->model_view_map({
+        gyle_id           => 'gyle_id',
+        company_id        => 'company_id',
+        product_id        => 'product_id',
+        abv               => 'abv',
+        comment           => 'comment',
+        ext_reference     => 'external_reference',
+        int_reference     => 'internal_reference',
+    });
+}
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched BeerFestDB::Web::Controller::Gyle in Gyle.');
+}
+
+=head2 list
+
+=cut
+
+sub list_by_festival_product : Local {
+
+    my ( $self, $c, $id ) = @_;
+
+    my $fp = $c->model( 'DB::FestivalProduct' )
+               ->find({ festival_product_id => $id });
+
+    my $rs = $c->model( 'DB::Cask' )
+               ->search({ festival_id          => $fp->get_column('festival_id'),
+                          'gyle_id.product_id' => $fp->get_column('product_id') },
+                        { join => { gyle_id => 'product_id' } })
+               ->search_related('gyle_id', undef, { distinct => 1 });
+
+    $self->generate_json_and_detach( $c, $rs );
+}
+
+=head2 submit
+
+=cut
+
+sub submit : Local {
+
+    my ( $self, $c ) = @_;
+
+    my $rs = $c->model( 'DB::Gyle' );
+
+    $self->write_to_resultset( $c, $rs );
+}
+
+=head2 delete
+
+=cut
+
+sub delete : Local {
+
+    my ( $self, $c ) = @_;
+
+    my $rs = $c->model( 'DB::Gyle' );
+
+    $self->delete_from_resultset( $c, $rs );
+}
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Tim F. Rayner
+
+This library is released under version 3 of the GNU General Public
+License (GPL).
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
--- trunk/root/static/js/grids/festival_product_view.js
+++ trunk/root/static/js/grids/festival_product_view.js
@@ -33,6 +33,17 @@ Ext.onReady(function(){
                      { name: 'name',       type: 'string'}]
     });
     company_store.load();
+    var distributor_combo = new Ext.form.ComboBox({
+        forceSelection: true,
+        allowBlank:     false,
+        typeAhead:      true,
+        triggerAction:  'all',
+        store:          company_store,
+        valueField:     'company_id',
+        displayField:   'name',
+        lazyRender:     true,
+        listClass:      'x-combo-list-small',
+    });
 
     /* Product drop-down */
     var product_store = new Ext.data.JsonStore({
@@ -61,10 +72,79 @@ Ext.onReady(function(){
                      { name: 'currency_code', type: 'string' }]
     });
     currency_store.load();
+    var currency_combo = new Ext.form.ComboBox({
+        forceSelection: true,
+        allowBlank:     false,
+        typeAhead:      true,
+        triggerAction:  'all',
+        store:          currency_store,
+        valueField:     'currency_id',
+        displayField:   'currency_code',
+        lazyRender:     true,
+        listClass:      'x-combo-list-small',
+    });
 
+    /* Casksize drop-down */
+    var casksize_store = new Ext.data.JsonStore({
+        url:        url_cask_size_list,
+        root:       'objects',
+        fields:     [{ name: 'container_size_id',   type: 'int'    },
+                     { name: 'description', type: 'string' }]
+    });
+    casksize_store.load();
+    var casksize_combo = new Ext.form.ComboBox({
+        forceSelection: true,
+        allowBlank:     false,
+        typeAhead:      true,
+        triggerAction:  'all',
+        store:          casksize_store,
+        valueField:     'container_size_id',
+        displayField:   'description',
+        lazyRender:     true,
+        listClass:      'x-combo-list-small',
+    });
+
+    /* Gyle drop-down */
+    var gyle_store = new Ext.data.JsonStore({
+        url:        url_gyle_list,
+        root:       'objects',
+        fields:     [{ name: 'gyle_id',       type: 'int'    },
+                     { name: 'int_reference', type: 'string' }]
+    });
+    gyle_store.load();
+    var gyle_combo = new Ext.form.ComboBox({
+        forceSelection: true,
+        allowBlank:     false,
+        typeAhead:      true,
+        triggerAction:  'all',
+        store:          gyle_store,
+        valueField:     'gyle_id',
+        displayField:   'int_reference',
+        lazyRender:     true,
+        listClass:      'x-combo-list-small',
+    });
+
+    /* Cask drop-down */
+    var cask_store = new Ext.data.JsonStore({
+        url:        url_cask_list,
+        root:       'objects',
+        fields:     [{ name: 'cask_id',           type: 'int' },
+                     { name: 'festival_id',       type: 'int' },
+                     { name: 'gyle_id',           type: 'int' },
+                     { name: 'distributor_id',    type: 'int' },
+                     { name: 'container_size_id', type: 'int' },
+                     { name: 'currency_id',       type: 'int' },
+                     { name: 'price',             type: 'int' },
+                     { name: 'int_reference',     type: 'string' },
+                     { name: 'ext_reference',     type: 'string' },
+                     { name: 'comment',           type: 'string' }]
+    });
+    cask_store.load();
+
+    /* FestivalProduct form */
     var fpForm = new MyFormPanel({
 
-        url:         url_object_submit,
+        url:         url_festivalproduct_submit,
         title:       'Festival Product details',
             
         items: [
@@ -134,12 +214,93 @@ Ext.onReady(function(){
         waitMsg:     'Loading Festival Product details...',
     });
 
+    /* Cask grid */
+    var caskGrid = new MyEditorGrid(
+        {
+            objLabel:           'Cask',
+            idField:            'cask_id',
+            autoExpandColumn:   'int_reference',
+            deleteUrl:          url_cask_delete,
+            submitUrl:          url_cask_submit,
+            recordChanges:      function (record) {
+                var fields = record.getChanges();
+                fields.cask_id = record.get( 'cask_id' );
+                fields.festival_id = festival_id;
+                fields.product_id  = product_id;
+                return(fields);
+            },
+            store:              cask_store,
+            contentCols: [
+                { id:        'int_reference',
+                  header:    'Festival Cask ID',
+                  dataIndex: 'int_reference',
+                  width:      50,
+                  editor:     new Ext.form.TextField({
+                      allowBlank: true,
+                  })},
+                { id:        'ext_reference',
+                  header:    'Brewery ID',
+                  dataIndex: 'ext_reference',
+                  width:      50,
+                  editor:     new Ext.form.TextField({
+                      allowBlank: true,
+                  })},
+                { id:         'container_size_id',
+                  header:     'Cask Size',
+                  dataIndex:  'container_size_id',
+                  width:      50,
+                  renderer:   MyComboRenderer(casksize_combo),
+                  editor:     casksize_combo, },
+                { id:         'price',
+                  header:     'Price',
+                  dataIndex:  'price',
+                  width:      30,
+                  editor:     new Ext.form.TextField({
+                      allowBlank:     true,
+                  })},
+                { id:         'currency_id',
+                  header:     'Currency',
+                  dataIndex:  'currency_id',
+                  width:      40,
+                  renderer:   MyComboRenderer(currency_combo),
+                  editor:     currency_combo, },
+                { id:         'gyle_id',
+                  header:     'Gyle (Festival ID)',
+                  dataIndex:  'gyle_id',
+                  width:      50,
+                  renderer:   MyComboRenderer(gyle_combo),
+                  editor:     gyle_combo, },
+                { id:         'distributor_id',
+                  header:     'Distributor',
+                  dataIndex:  'distributor_id',
+                  width:      130,
+                  renderer:   MyComboRenderer(distributor_combo),
+                  editor:     distributor_combo, },
+                { id:        'comment',
+                  header:    'Comment',
+                  dataIndex: 'comment',
+                  editor:     new Ext.form.TextField({
+                      allowBlank: true,
+                  })},
+            ],
+            viewLink: function (grid, record, action, row, col) {
+                var t = new Ext.XTemplate('/cask/view/{cask_id}');
+                window.location=t.apply({
+                    cask_id: record.get('cask_id'),
+                })
+            },
+        }
+    );
+
     var tabpanel = new Ext.TabPanel({
         activeTab: 0,
         items: [
             { title: 'Festival Product Information',
               layout: 'anchor',
               items:  fpForm, },
+            { title: 'Casks',
+              layout: 'fit',
+              items:  caskGrid, },
         ],
     });
 
