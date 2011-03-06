299108b3d4a45217ef520f95081cd874e99c403c
Tim Rayner <tfrayner@users.noreply.github.com>
2011-03-06 17:09:12 +0000

    Rough draft of the new loader.

--- lib/BeerFestDB/Loader.pm
+++ lib/BeerFestDB/Loader.pm
@@ -129,7 +129,6 @@ sub next {
 }
 
 
-
 package BeerFestDB::Loader;
 
 use Moose;
@@ -156,16 +155,145 @@ sub load {
     return;
 }
 
-{
+{ # BEGIN LOAD_DATA SCOPE
 
-    my %used;
+my ( %KeyUsed, %ObjCache, %ClassUsed );
 
-    sub _load_data {  # Recursive method.
+sub _load_data {
 
-        my ( $self, $row ) = @_;
+    my ( $self, $row ) = @_;
 
+    # We need a loop at the per-row level: while any unused keys
+    # are left, repeat _load_row_data.
+    %KeyUsed = map { $_ => 0 } keys %$row;
+    while ( any { $_ == 0 } values %KeyUsed ) {
+        $self->_load_row_data( $row );
+    }
+}
+
+sub _list_unused {
+    
+    my ( $self ) = @_;
+    
+    my %check  = grep { $KeyUsed{$_} == 0 } keys %KeyUsed;
+    my @unused = sort keys %check;
+
+    return \@unused;
+}
+
+sub _load_row_data {  # Recursive method.
+
+    my ( $self, $row, $class ) = @_;
+
+    my ( $rs, $obj );
+
+    DATAKEY:
+    while ( ! defined $class ) {
+
+        # Find first unused key, select that as the class.
+        my $unused = $self->_list_unused();
+            
+        # If no unused keys left, return.
+        last DATAKEY if ( scalar @{ $unused } == 0 );
+
+        # Arbitrarily choose a column key.
+        my $next = $unused->[0];
+
+        # Make sure the class exists in the database; if not, try
+        # the next key, raise a warning and set the key to
+        # used.
+        my ( $cand, $attr ) = $self->_split_colname( $next );
+        $rs = $self->database->resultset($cand);
+
+        unless ( defined $cand && defined $attr && defined $rs ) {
+
+            # FIXME might want to limit warnings to once per run, not once per row.
+            warn(qq{Warning: unrecognised column name "$next"\n});
+            $KeyUsed{ $next }++;
+            next DATAKEY;
+        }
     }
 
+    return unless defined $class;
+
+    # Simple check for cycles in recursion. This really shouldn't happen.
+    if ( $ClassUsed{ $class }++ ) {
+        die("Error: cyclic dependency detected in the database schema.\n");
+    }
+
+    # Pull out all the keys relating to $class.
+    my ( %obj_attr, @keys_used );
+    COLUMN:
+    foreach my $key ( @{ $self->_list_unused() } ) {
+        my ( $key_class, $key_attr ) = $self->_split_colname( $key );
+        
+        unless ( defined $key_class && defined $key_attr ) {
+
+            # FIXME might want to limit warnings to once per run, not once per row.
+            warn(qq{Warning: unrecognised column name "$key"\n});
+            $KeyUsed{ $key }++;
+            next COLUMN;
+        }
+        
+        next COLUMN unless $key_class eq $class;
+
+        # Check that $key_attr is a valid $key_class attr FIXME
+        unless ( $rs->result_source->has_column( $key_attr ) ) {
+            croak(qq{Error: Database class "$class" has no column named "$key_attr".\n});
+        }
+
+        $obj_attr{ $key_attr } = $row->{ $key_attr };
+        $KeyUsed{ $key }++;
+    }
+
+    # If any of the object database attributes correspond to
+    # unused keys, check if the keys have been used. If so, pull
+    # out the object from the cache (keyed by target class) and
+    # dump them in %obj_hash; otherwise, recurse down a level and
+    # create those objects.
+    foreach my $relname ( $rs->result_source()->relationships() ) {
+        my $target_class = $rs->result_source()->related_class( $relname );
+        my $target;
+        unless ( my $target = $ObjCache{$target_class} ) {
+
+            # check unused row keys for $target_class
+            foreach my $key ( @{ $self->_list_unused() } ) {
+                my ( $key_class, $key_attr ) = $self->_split_colname( $key );
+                if ( $key_class eq $target_class ) {
+
+                    # Recurse here.
+                    $target = $self->_load_row_data( $row, $target_class );
+                }
+            }
+        }
+        $obj_attr{ $relname } = $target if defined $target;
+    }
+
+    # See if all the required attributes for this $class are
+    # present.
+    my ( $req, $opt ) = $self->_find_required_cols( $class );
+    unless ( $self->_confirm_required_cols( \%obj_attr, $req ) ) {
+        croak(qq{Error: Missing required attributes for class "$class": }
+                  . join(", ", @$req) . "\n");
+    }
+
+    # Actually create the object belonging to $class FIXME. Also
+    # put it into %ObjCache.
+    $obj = $rs->find_or_create( \%obj_attr );
+    $ObjCache{ $class } = $obj;
+
+    return $obj;
+}
+
+} # END LOAD_DATA SCOPE
+
+sub _split_colname {
+
+    my ( $self, $colname ) = @_;
+
+    my @parts = split /[.-|: ]+/, $colname, 2;
+
+    return @parts;
 }
 
 sub _check_not_null {
@@ -221,265 +349,6 @@ sub _confirm_required_cols {
     else {
 	return 1;
     }
-}
-
-__END__
-
-sub _load_data {
-
-    my ( $self, $datahash ) = @_;
-
-    # Each of these calls defines the column to be used from the input
-    # file.
-    my $festival
-	= $self->_check_not_null( $datahash->{$FESTIVAL_YEAR} )
-	? $self->_load_column_value(
-	    {
-		year        => $datahash->{$FESTIVAL_YEAR},
-		name        => $datahash->{$FESTIVAL_NAME},
-                description => $datahash->{$FESTIVAL_DESCRIPTION},
-	    },
-	    'Festival')
-	: die("Error: Must have full festival info information (i.e. year)");
-
-    my $stillage
-	= $self->_check_not_null( $datahash->{$STILLAGE_LOCATION} )
-	? $self->_load_column_value(
-	    {
-		description => $datahash->{$STILLAGE_LOCATION},
-                festival_id => $festival->id,
-	    },
-	    'StillageLocation')
-	: undef;
-
-    my $bar
-	= $self->_check_not_null( $datahash->{$BAR_DESCRIPTION} )
-	? $self->_load_column_value(
-	    {
-		description => $datahash->{$BAR_DESCRIPTION},
-                festival_id => $festival->id,
-	    },
-	    'Bar')
-	: undef;
-
-    # FIXME no addresses at this point.
-    my $brewer
-	= $self->_check_not_null( $datahash->{$BREWER_NAME} )
-	? $self->_load_column_value(
-	    {
-		name         => $datahash->{$BREWER_NAME},
-		loc_desc     => $datahash->{$BREWER_LOC_DESC},
-		year_founded => $datahash->{$BREWER_YEAR_FOUNDED},
-		comment      => $datahash->{$BREWER_COMMENT},
-	    },
-	    'Company')
-	: undef;
-
-    my $category
-        = $self->_load_column_value(
-            {
-                description => $datahash->{$PRODUCT_CATEGORY} || 'beer',
-            },
-            'ProductCategory');
-
-    # FIXME this is a controlled vocab so should die on invalid values.
-    my $style
-	= $self->_check_not_null( $datahash->{$PRODUCT_STYLE} )
-	? $self->_load_column_value(
-	    {
-                product_category_id => $category,
-		description         => $datahash->{$PRODUCT_STYLE},
-	    },
-	    'ProductStyle')
-	: undef;
-
-    # Likely to be the default for UK beer festivals.
-    my $pint_size = $self->_load_column_value(
-        {
-            litre_multiplier => 0.5682,
-            description      => 'pint',
-        },
-        'ContainerMeasure');
-
-    # FIXME why are we reiterating the description here?
-    my $sale_volume = $self->_load_column_value(
-        {
-            container_measure_id    => $pint_size,
-            sale_volume_description => 'pint',
-        },
-        'SaleVolume');
-
-    # FIXME this also needs to be much more flexible (see http://en.wikipedia.org/wiki/ISO_4217).
-    my $currency = $self->_load_column_value(
-        {
-            currency_code   => 'GBP',
-            currency_number => 826,
-            currency_format => '#,###,###,###,##0.00',
-            exponent        => 2,
-            currency_symbol => '£',
-        },
-        'Currency');
-
-    my $sale_price = $datahash->{$GYLE_PINT_PRICE} ? $datahash->{$GYLE_PINT_PRICE} * 100 : undef;
-
-    my $nominal_abv = $datahash->{$PRODUCT_ABV};
-    $nominal_abv = undef if ( defined $nominal_abv && $nominal_abv eq q{} );
-    my $product
-	= $self->_check_not_null( $datahash->{$PRODUCT_NAME} )
-	? $self->_load_column_value(
-	    {
-		name             => $datahash->{$PRODUCT_NAME},
-                company_id       => $brewer->company_id,
-		description      => $datahash->{$PRODUCT_DESCRIPTION},
-		comment          => $datahash->{$PRODUCT_COMMENT},
-                product_category_id => $category,
-                product_style_id    => $style,
-		nominal_abv         => $nominal_abv,
-	    },
-	    'Product')
-	: undef;
-
-    my $festival_product;
-    if ( $product && $festival ) {
-        $festival_product = $self->_load_column_value(
-            {
-                festival_id => $festival->festival_id,
-                product_id  => $product->product_id,
-                sale_volume_id      => $sale_volume,
-                sale_currency_id    => $currency,
-                sale_price          => $sale_price,
-            },
-            'FestivalProduct');
-    }
-
-    my $gyle
-	= $festival_product
-	? $self->_load_column_value(
-	    {
-		external_reference => $datahash->{$GYLE_BREWERY_NUMBER},
-                internal_reference => 'auto-generated',
-		company_id         => $brewer,
-		festival_product_id => $festival_product,
-		abv                => $datahash->{$GYLE_ABV},
-		comment            => $datahash->{$GYLE_COMMENT},
-	    },
-	    'Gyle')
-	: undef;
-
-    my $distributor
-	= $self->_check_not_null( $datahash->{$DISTRIBUTOR_NAME} )
-	? $self->_load_column_value(
-	    {
-		name         => $datahash->{$DISTRIBUTOR_NAME},
-		loc_desc     => $datahash->{$DISTRIBUTOR_LOC_DESC},
-		year_founded => $datahash->{$DISTRIBUTOR_YEAR_FOUNDED},
-		comment      => $datahash->{$DISTRIBUTOR_COMMENT},
-	    },
-	    'Company')
-	: undef;
-
-    # This is going to be pretty much constant for UK beers. Will need
-    # modification for European casks though FIXME.
-    my $cask_measure = $self->_load_column_value(
-        {
-            litre_multiplier => 4.54609188,
-            description      => 'gallon',
-        },
-        'ContainerMeasure');
-
-    my $cask_size
-        = $self->_check_not_null( $datahash->{$CASK_SIZE} )
-        ? $self->_load_column_value(
-            {
-                container_volume     => $datahash->{$CASK_SIZE},
-                container_measure_id => $cask_measure,
-            },
-            'ContainerSize')
-        : undef;
-
-    my $cask_price = $datahash->{$CASK_PRICE}      ? $datahash->{$CASK_PRICE}      * 100 : undef;
-
-    my $count = $datahash->{$CASK_COUNT};
-    unless ( defined $count && $count ne q{} ) {
-        $count = 1;
-    }
-
-    # We need to support adding casks in multiple loads; cask count
-    # becomes an issue so we check against the database here.
-    my $preexist = 0;
-    if ( $gyle ) { 
-	$preexist = $gyle->search_related(
-	    'casks',
-	    { 'festival_id' => $festival->id })->count();
-	$count += $preexist;
-    }
-
-    foreach my $n ( ($preexist+1)..$count ) {
-
-        my $cask
-            = $product
-                ? $self->_load_column_value(
-                    {
-                        gyle_id                => $gyle,
-                        distributor_company_id => $distributor,
-                        festival_id            => $festival,
-                        container_size_id      => $cask_size,
-                        currency_id            => $currency,
-                        price                  => $cask_price,
-                        stillage_location_id   => $stillage,
-                        bar_id                 => $bar,
-                        comment                => $datahash->{$CASK_COMMENT},
-                        internal_reference     => $n,
-                    },
-                    'Cask')
-                    : undef;
-
-        # FIXME at the moment we're assuming that dip measurements use the
-        # same volume units as the cask sizes.
-        my $cask_measurement
-            = $self->_check_not_null( $datahash->{$CASK_MEASUREMENT_VOLUME} )
-                ? $self->load_cask_measurement(
-                    {
-                        cask_id              => $cask,
-                        date                 => $datahash->{$CASK_MEASUREMENT_DATE},
-                        volume               => $datahash->{$CASK_MEASUREMENT_VOLUME},
-                        container_measure_id => $cask_measure,
-                        comment              => $datahash->{$CASK_MEASUREMENT_COMMENT},
-	    },
-                    'CaskMeasurement')
-                    : undef;
-    }
-
-    return;
-}
-
-sub _load_column_value {
-
-    my ( $self, $args, $class, $trigger ) = @_;
-
-    my $resultset = $self->database()->resultset($class)
-	or confess(qq{Error: No result set returned from DB for class "$class".});
-
-    foreach my $key ( keys %$args ) {
-        delete $args->{$key} if ( ! defined $args->{$key} || $args->{$key} eq q{} );
-    }
-
-    # Validate our arguments against the database.
-    my ( $required, $optional ) = $self->_find_required_cols( $resultset );
-    my @pk = $resultset->result_source()->primary_columns();
-    my %recognised = map { $_ => 1 } @{ $required }, @{ $optional }, @pk;
-    foreach my $key ( keys %{ $args } ) {
-	unless ( $recognised{ $key } ) {
-	    confess(qq{Error: Unrecognised column key "$key".}); 
-	}
-    }
-    $self->_confirm_required_cols( $args, $required )
-	or croak(qq{Error: Incomplete data for "$class" object.});
-
-    # Create an object in the database.
-    my $object = $resultset->update_or_create($args);
-
-    return $object;
 }
 
 =head1 COPYRIGHT AND LICENSE
