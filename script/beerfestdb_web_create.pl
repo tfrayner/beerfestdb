2c48298a7e6c4cc3f99496d03ff264f4a8e69451
Tim Rayner <tfrayner@users.noreply.github.com>
2011-04-07 08:19:22 +0000

    Automated update of Catalyst scripts.

--- script/beerfestdb_web_create.pl
+++ script/beerfestdb_web_create.pl
@@ -1,26 +1,10 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use Catalyst::Helper;
 
-my $force = 0;
-my $mech  = 0;
-my $help  = 0;
-
-GetOptions(
-    'nonew|force'    => \$force,
-    'mech|mechanize' => \$mech,
-    'help|?'         => \$help
- );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-my $helper = Catalyst::Helper->new( { '.newfiles' => !$force, mech => $mech } );
-
-pod2usage(1) unless $helper->mk_component( 'BeerFestDB::Web', @ARGV );
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('BeerFestDB::Web', 'Create');
 
 1;
 
@@ -33,13 +17,12 @@ beerfestdb_web_create.pl - Create a new Catalyst Component
 beerfestdb_web_create.pl [options] model|view|controller name [helper] [options]
 
  Options:
-   -force        don't create a .new file where a file to be created exists
-   -mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
-   -help         display this help and exits
+   --force        don't create a .new file where a file to be created exists
+   --mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
+   --help         display this help and exits
 
  Examples:
    beerfestdb_web_create.pl controller My::Controller
-   beerfestdb_web_create.pl controller My::Controller BindLex
    beerfestdb_web_create.pl -mechanize controller My::Controller
    beerfestdb_web_create.pl view My::View
    beerfestdb_web_create.pl view MyView TT
@@ -68,7 +51,7 @@ Catalyst Contributors, see Catalyst.pm
 
 =head1 COPYRIGHT
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
