eb9b4fedaa7bd7ea3c90293e75b7033b82e536c7
Tim Rayner <tfrayner@users.noreply.github.com>
2012-05-19 11:27:15 +0000

    Check that the CSV parsing has completed properly.

--- util/load_dips.pl
+++ util/load_dips.pl
@@ -155,6 +155,16 @@ if ( $@ ) {
     die(qq{Errors encountered during load:\n\n$@});
 }
 else {
+    
+    # Check that parsing completed successfully.
+    my ( $error, $mess ) = $csv_parser->error_diag();
+    unless ( $error == 2012 ) {    # 2012 is the Text::CSV_XS EOF code.
+	die(sprintf(
+		"Error in tab-delimited format: %s. Bad input was:\n\n%s\n",
+		$mess,
+		$csv_parser->error_input()));
+    }
+
     print("Dip data successfully loaded.\n");
 }
 
