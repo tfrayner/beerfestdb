66ffd69849dc300937bce0a7cf563e3b3773c041
Tim Rayner <tfrayner@users.noreply.github.com>
2013-04-27 10:29:00 +0000

    Adding the ability to update vented, tapped, ready, condemned flags using the update_cask_details.pl script.

--- util/update_cask_details.pl
+++ util/update_cask_details.pl
@@ -25,15 +25,15 @@ use warnings;
 
 use Getopt::Long;
 use Pod::Usage;
-use Text::CSV_XS;
-use List::Util qw(first);
-use Scalar::Util qw(looks_like_number);
 use BeerFestDB::ORM;
 use BeerFestDB::Web;
 
 package CaskUpdater;
 
 use Moose;
+use List::Util qw(first);
+use Scalar::Util qw(looks_like_number);
+use Text::CSV_XS;
 
 has 'database'  => ( is       => 'ro',
                      isa      => 'DBIx::Class::Schema',
@@ -102,9 +102,9 @@ sub assign_stillage_location {
     return;
 }
 
-sub update_cask {
+sub update_caskman {
 
-    my ( $self, $cask, $row ) = @_;
+    my ( $self, $caskman, $row ) = @_;
 
     # First deal with stillage location. This is required
     # to prevent one well-meaning cellar person from
@@ -113,11 +113,11 @@ sub update_cask {
     if ( ! defined $loc ) {
         die("Error: stillage_location not defined.");
     }
-    $self->assign_stillage_location( $cask, $loc );
+    $self->assign_stillage_location( $caskman, $loc );
     
     # Cellar id ("internal_reference").
     if ( my $id = $row->{ 'cask_cellar_id' } ) {
-        $self->assign_cellar_number($cask, $id);
+        $self->assign_cellar_number($caskman, $id);
     }
     
     # Stillage bay.
@@ -126,7 +126,7 @@ sub update_cask {
             if ( ! looks_like_number( $bay ) ) {
                 die("Error: This stillage_bay doesn't look like a number: $bay\n");
             }
-            $cask->set_column('stillage_bay', $bay);
+            $caskman->set_column('stillage_bay', $bay);
         }
     }
 
@@ -135,11 +135,29 @@ sub update_cask {
         my $pos = $self->database->resultset('BayPosition')
             ->find({ description => $baypos })
                 or die(qq{Error: Bay position "$baypos" not found.});
-        $cask->set_column('bay_position_id',
+        $caskman->set_column('bay_position_id',
                           $pos->bay_position_id());
     }
-    
-    $cask->update();
+
+    # Vented, Tapped, Ready...
+    my %statmap = ( 'vented'    => 'is_vented',
+                    'tapped'    => 'is_tapped',
+                    'ready'     => 'is_ready',
+                    'condemned' => 'is_condemned' );
+    my $cask = $caskman->casks()->first();
+    while ( my ( $col, $dbcol ) = each %statmap ) {
+        my $value = $row->{ $col };
+        if ( defined $value && $value ne q{} ) {
+            if ( ! $cask ) {
+                die("Attempting to set a concrete cask status"
+                        . " on a virtual cask (has it arrived yet?).")
+            }
+            $cask->set_column($dbcol, $value ? 1 : 0);
+        }
+    }
+
+    $cask->update() if $cask;
+    $caskman->update();
 
     return;
 }
@@ -179,7 +197,8 @@ sub load {
     foreach my $col ( @header ) {
         unless ( first { $col eq $_ } qw(cask_festival_id cask_cellar_id
                                          stillage_location stillage_bay
-                                         bay_position) ) {
+                                         bay_position vented tapped ready
+                                         condemned) ) {
             die("Unrecognised column heading: $col\n");
         }
     }
@@ -203,13 +222,13 @@ sub load {
                         next CASK;
                     }
                     
-                    my $cask = $self->database->resultset('Cask')
+                    my $caskman = $self->database->resultset('CaskManagement')
                         ->find({ festival_id      => $self->festival->id(),
                                  cellar_reference => $cfid })
-                            or die(qq{Error: Cask with cellar_reference "$cfid" }
+                            or die(qq{Error: CaskManagement with cellar_reference "$cfid" }
                                        . qq{not found.\n});
 
-                    $self->update_cask( $cask, \%row );
+                    $self->update_caskman( $caskman, \%row );
                 }
                 
                 # Useful for reporting discrepancies back to cellar managers.
@@ -222,7 +241,7 @@ sub load {
         );
     };
     if ( $@ ) {
-        die(qq{Errors encountered during load:\n\n$@});
+        die(qq{Errors encountered during load (database not changed):\n\n$@});
     }
     else {
         
@@ -308,35 +327,58 @@ moved between stillages (default: no).
 
 =head2 -f
 
-The file containing cask details. This should be in tab-delimited format and contain the following headings:
+The file containing cask details. This should be in tab-delimited
+format and contain the following headings:
 
 =over 2
 
 =item cask_festival_id
 
-The unique identifier for the cask within the festival.
+REQUIRED. The unique identifier for the cask within the festival.
 
 =item cask_cellar_id
 
-The cask cellar number; i.e. the number of the cask within the gyle,
-typically in order of sale. If you have six casks of a given beer,
-they will typically be numbered 1 to 6.
+OPTIONAL. The cask cellar number; i.e. the number of the cask within the gyle,
+typically in order of sale. For example, if you have six casks of a given beer,
+they would most likely be numbered 1 to 6 in this column.
 
 =item stillage_location
 
-The name of the stillage upon which the cask rests. These locations
-must already have been registered in the database.
+REQUIRED. The name of the stillage upon which the cask rests. These
+locations must already have been registered in the database. Note that
+this column is required specifically to avoid accidental cross-talk
+between cellar teams.
 
 =item stillage_bay
 
-The number of the bay on the stillage. Currently only constrained to
+OPTIONAL. The number of the bay on the stillage. Currently only constrained to
 being an integer.
 
 =item bay_position
 
-The position of the cask within the bay. This must conform to the
+OPTIONAL. The position of the cask within the bay. This must conform to the
 standard list registered in the bay_position table (e.g. "Top Front",
 "Bottom Back").
+
+=item vented
+
+OPTIONAL. Indicates whether the cask has been vented. Code this as '1'
+(or any 'true' value in perl, i.e. not zero) to indicate that the cask
+has been vented. A zero ('0') in this column will be interpreted as
+indicating that the cask is not vented; this can be used to correct
+errors in the database. A blank value will be ignored.
+
+=item tapped
+
+OPTIONAL. Whether the cask has been tapped or not. See the notes under 'vented' for details.
+
+=item ready
+
+OPTIONAL. Whether the cask is ready to serve. See the notes under 'vented' for details.
+
+=item condemned
+
+OPTIONAL. Whether the cask has been condemned. See the notes under 'vented' for details.
 
 =back
 
@@ -346,7 +388,7 @@ Tim F. Rayner, E<lt>tfrayner@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2012 by Tim F. Rayner
+Copyright (C) 2012-2013 by Tim F. Rayner
 
 This library is released under version 3 of the GNU General Public
 License (GPL).
