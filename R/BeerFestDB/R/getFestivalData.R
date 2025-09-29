2c7f22ee952e74c0900d7829a7780f242cdd4c53
Tim Rayner <tfrayner@gmail.com>
2025-09-29 18:17:15 +0000

    Rewrite getFestivalData using tidyverse

--- R/BeerFestDB/R/getFestivalData.R
+++ R/BeerFestDB/R/getFestivalData.R
@@ -23,25 +23,27 @@ getFestivalData <- function(baseuri, festname, prodcat, auth = NULL, .opts = lis
   }
 
   ## Begin building the main data frame.
-  fest <- getBFData(
+  festival_id <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "Festival", "list"
-  )
-  festival_id <- fest[fest$name == festname, "festival_id"]
+  ) %>%
+    subset(name == festname) %>%
+    pull(festival_id)
 
   batch <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "MeasurementBatch", "list",
     params = festival_id
-  )
-  batch$measurement_time <- as.Date(batch$measurement_time)
-  batch <- batch[order(batch$measurement_time), ]
+  ) %>%
+    mutate(measurement_time = as.Date(measurement_time)) %>%
+    arrange(measurement_time)
 
-  cat <- getBFData(
+  prodcat_id <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "ProductCategory", "list"
-  )
-  prodcat_id <- cat[cat$description == prodcat, "product_category_id"]
+  ) %>%
+    subset(description == prodcat) %>%
+    pull(product_category_id)
 
   cask <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
@@ -53,47 +55,52 @@ getFestivalData <- function(baseuri, festname, prodcat, auth = NULL, .opts = lis
       "festival_ref", "is_condemned", "is_sale_or_return",
       "price", "comment"
     )
-  )
-
-  w <- !colnames(cask) %in% c("comment", "price")
-  suppressWarnings(cask[, w] <- as.data.frame(apply(cask[, w], 2, as.integer)))
-  cask[is.na(cask$is_condemned), "is_condemned"] <- 0
-  cask[is.na(cask$is_sale_or_return), "is_sale_or_return"] <- 0
-  cask[is.na(cask$comment), "comment"] <- ""
+  ) %>%
+    rename(cask_price = "price") %>%
+    mutate(
+      festival_ref = as.integer(festival_ref),
+      is_condemned = as.integer(is_condemned),
+      is_sale_or_return = as.integer(is_sale_or_return)
+    ) %>%
+    replace_na(list(is_condemned = 0, is_sale_or_return = 0, comment = ""))
 
   # FIXME we want to map between sale_price_currency and cask_price_currency, with similar treatment for sale_volume and cask_volume as well. Skipped for now for speed.
-  fp <- getBFData(
-    baseuri = baseuri, auth = auth, .opts = .opts,
-    "FestivalProduct", "list",
-    params = c(festival_id, prodcat_id),
-    columns = c(
-      "product_id",
-      "sale_price"
+  cask <- cask %>%
+    left_join(
+      getBFData(
+        baseuri = baseuri, auth = auth, .opts = .opts,
+        "FestivalProduct", "list",
+        params = c(festival_id, prodcat_id),
+        columns = c(
+          "product_id",
+          "sale_price"
+        )
+      ),
+      by = "product_id"
     )
-  )
 
-  cask <- merge(cask, fp, by = "product_id")
-
-  measures <- getBFData(
+  default_cask_measure <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "ContainerMeasure", "list",
     columns = c("description", "litre_multiplier")
-  )
-  default_cask_measure <- as.numeric(with(measures, litre_multiplier[description == "gallon"]))
+  ) %>%
+    subset(description == "gallon") %>%
+    pull(litre_multiplier) %>%
+    as.numeric()
   stopifnot(length(default_cask_measure) == 1)
 
   sizes <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "ContainerSize", "list",
     columns = c("container_size_id", "volume", "litre_multiplier", "description")
-  )
-  colnames(sizes)[2] <- "cask_volume"
-  for (n in 2:3) {
-    sizes[, n] <- as.numeric(sizes[, n])
-  }
-  sizes$cask_volume <- with(sizes, cask_volume * litre_multiplier) / default_cask_measure
-  sizes <- sizes[, 1:2]
-  cp <- merge(cask, sizes, by = "container_size_id")
+  ) %>%
+    rename(cask_volume = "volume", size_name = "description") %>%
+    mutate(litre_multiplier = as.numeric(litre_multiplier)) %>%
+    mutate(cask_volume = as.numeric(cask_volume) * litre_multiplier / default_cask_measure) %>%
+    select(container_size_id, cask_volume, size_name)
+
+  cp <- cask %>%
+    left_join(sizes, by = "container_size_id")
 
   product <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
@@ -103,70 +110,79 @@ getFestivalData <- function(baseuri, festname, prodcat, auth = NULL, .opts = lis
       "product_id", "company_id", "nominal_abv",
       "name", "product_style_id"
     )
-  )
-  colnames(product)[4] <- "product_name"
-  cp <- merge(cp, product, by = "product_id")
+  ) %>%
+    rename(product_name = "name")
+  cp <- cp %>%
+    left_join(product, by = "product_id")
 
   gyle <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "Gyle", "list_by_festival",
     params = festival_id,
     columns = c("gyle_id", "abv")
-  )
-  colnames(gyle)[2] <- "gyle_abv"
-  cp <- merge(cp, gyle, by = "gyle_id")
+  ) %>%
+    rename(gyle_abv = "abv")
+  cp <- cp %>%
+    left_join(gyle, by = "gyle_id")
 
   ## Sort out ABVs. If a gyle ABV is present, use it preferentially.
   suppressWarnings(cp$nominal_abv <- as.numeric(cp$nominal_abv))
   suppressWarnings(cp$gyle_abv <- as.numeric(cp$gyle_abv))
-  cp$abv <- ifelse(is.na(cp$gyle_abv), cp$nominal_abv, cp$gyle_abv)
-  cp <- cp[, !colnames(cp) %in% c("nominal_abv", "gyle_abv")]
+  cp <- cp %>%
+    mutate(abv = ifelse(is.na(gyle_abv), nominal_abv, gyle_abv)) %>%
+    select(-any_of(c("nominal_abv", "gyle_abv")))
 
   style <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "ProductStyle", "list",
     columns = c("product_style_id", "description")
-  )
-  colnames(style)[2] <- "style"
-  cp <- merge(cp, style, by = "product_style_id", all.x = TRUE)
+  ) %>%
+    rename(style = "description")
+  cp <- cp %>%
+    left_join(style, by = "product_style_id")
 
   company <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "company", "list",
     columns = c("company_id", "name", "company_region_id")
-  )
-  colnames(company)[2] <- "company_name"
-  cp <- merge(cp, company, by = "company_id")
+  ) %>%
+    rename(company_name = "name")
+  cp <- cp %>%
+    left_join(company, by = "company_id")
 
   region <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "CompanyRegion", "list",
     columns = c("company_region_id", "description")
-  )
-  colnames(region)[2] <- "region"
-  cp <- merge(cp, region, by = "company_region_id", all.x = TRUE)
+  ) %>%
+    rename(region = "description")
+  cp <- cp %>%
+    left_join(region, by = "company_region_id")
 
   stillage <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "StillageLocation", "list",
     params = festival_id,
     columns = c("stillage_location_id", "description")
-  )
-  colnames(stillage)[2] <- "stillage"
-  cp <- merge(cp, stillage, by = "stillage_location_id", all.x = TRUE)
+  ) %>%
+    rename(stillage = "description")
+  cp <- cp %>%
+    left_join(stillage, by = "stillage_location_id")
 
   orderbatch <- getBFData(
     baseuri = baseuri, auth = auth, .opts = .opts,
     "OrderBatch", "list",
     params = festival_id,
     columns = c("order_batch_id", "description")
-  )
-  colnames(orderbatch)[2] <- "order_batch"
-  cp <- merge(cp, orderbatch, by = "order_batch_id", all.x = TRUE)
-  cp[is.na(cp$order_batch), "order_batch"] <- "Other"
+  ) %>%
+    rename(order_batch = "description")
+  cp <- cp %>%
+    left_join(orderbatch, by = "order_batch_id") %>%
+    replace_na(list(order_batch = "Other"))
 
   ## Throw out all database ID columns except cask_id.
-  cp <- cp[, !grepl("(?<!cask)_id$", colnames(cp), perl = TRUE)]
+  cp <- cp %>%
+    select(-matches("(?<!cask)_id$", perl = TRUE))
 
   dipmat <- as.data.frame(matrix(NA, nrow = nrow(cp), ncol = nrow(batch)))
   rownames(dipmat) <- cp$cask_id
@@ -179,12 +195,10 @@ getFestivalData <- function(baseuri, festname, prodcat, auth = NULL, .opts = lis
     }))
     dipmat[as.character(id), ] <- d
   }
-  cp <- merge(cp, dipmat, by.x = "cask_id", by.y = 0, all.x = TRUE)
-
   ## Dip figures need to be numeric.
-  w <- grepl("^dip\\.", colnames(cp))
-  cp[, w] <- apply(cp[, w], 2, as.numeric)
-  cp$cask_volume <- as.numeric(cp$cask_volume)
+  dipmat <- apply(dipmat, c(1, 2), as.numeric)
+  cp <- merge(cp, dipmat, by.x = "cask_id", by.y = 0, all.x = TRUE) %>%
+    mutate(cask_volume = as.numeric(cask_volume))
 
   return(cp)
 }
