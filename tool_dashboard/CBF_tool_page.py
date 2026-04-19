2a93f68ea21c2e04d751d90c16bc0572ef9627fb
Tim Rayner <tfrayner@gmail.com>
2026-04-19 11:38:49 +0000

    Adding favicon to the tool dashboard

--- tool_dashboard/CBF_tool_page.py
+++ tool_dashboard/CBF_tool_page.py
@@ -2,6 +2,7 @@
 import streamlit as st
 
 st.title("BeerfestDB Tools")
+st.set_page_config(page_icon="favicon.ico")
 
 if not st.user.is_logged_in:
     if st.button("Log in with BeerfestDB account"):
