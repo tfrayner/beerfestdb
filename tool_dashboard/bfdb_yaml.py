669f2ed63ff769ed4c2a5ee65f6b1d33a6752fed
Midori Harris <mah79@cam.ac.uk>
2026-05-05 10:42:49 +0100

    little script to get items from yaml; relevant to #104

---
+++ tool_dashboard/bfdb_yaml.py
@@ -0,0 +1,14 @@
+import yaml
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+parent_dir = Path(this_dir).parent
+
+bfdbyaml = parent_dir / 'beerfestdb_web.yml'
+
+with open(f'{bfdbyaml}') as f:
+    bfdbconf = yaml.safe_load(f)
+
+current_festival = (bfdbconf["current_festival"])
+
+print(f'{current_festival}')
