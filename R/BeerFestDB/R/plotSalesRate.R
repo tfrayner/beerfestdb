947715f6e09a98843d0621f3c46bcee452d35cb4
Tim Rayner <tfrayner@gmail.com>
2026-07-24 18:49:50 +0000

    Added sales heatmap; fixing report layout for best use of space

--- R/BeerFestDB/R/plotSalesRate.R
+++ R/BeerFestDB/R/plotSalesRate.R
@@ -49,3 +49,34 @@ plotSalesRate <- function(festival, colname, ...) {
 
   plotFractions(dp / start[rownames(dp), "start"], ylim = c(0, NA), ylab = "Fraction Sold", ...)
 }
+
+###############################################################################
+#' Draw a heatmap of sales rates for a given set of categories over time
+#' @description Aggregates cask volume data by \code{colname}, scales the data
+#'   for each group to the same mean and variance, and draws a heatmap of the
+#'   result showing which sessions were outliers for each group.
+#' @param festival A data frame of cask/dip data, typically from the `data` slot of a Festival object returned by
+#'   \code{\link{getFestivalData}}.
+#' @param colname A character string naming the column in \code{festival$data} to use
+#'   as the grouping variable (e.g., \code{"region"}, \code{"style"}).
+#' @return Invisibly returns \code{NULL} (called for its side effect of producing a plot).
+#' @seealso \code{\link{plotSalesRate}}, \code{\link{analyseData}}
+#' @importFrom ggplot2 ggplot aes geom_tile scale_fill_gradient2 labs theme theme_minimal ggtitle element_text
+#' @importFrom reshape2 melt
+#' @importFrom tibble column_to_rownames
+#' @export
+###############################################################################
+plotSalesHeatmap <- function(festival, colname) {
+
+  festival <- with(festival$data, festival$subset(!is.na(get(colname))))
+
+  dp <- festival$grouped_per_diem_sales(colname) %>% 
+    column_to_rownames(colname)
+
+  ggplot(reshape2::melt(t(scale(t(dp)))), aes(x=as.numeric(Var2), y=Var1, fill=value)) +
+    geom_tile() + scale_fill_gradient2(low="blue", mid="white", high="red", midpoint=0) +
+    scale_x_continuous(breaks=1:ncol(dp), labels=colnames(dp)) +
+    labs(x='Festival Day', y='Volume Sold (gallons)') +
+    theme_minimal() + ggtitle(paste("Sales by", colname)) +
+    theme(legend.position='right', legend.title=element_text(size=12), legend.text=element_text(size=10))
+}
